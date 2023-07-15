db89103a2ded83efac380b514ed4a9db40435ff7
ibarretom <igorbm.23@gmail.com>
2023-07-15 14:18:56 -0300

    Vuetify added

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <header class="bg-red">
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
+      <v-text-field label="hello world" rounded variant="outlined"></v-text-field>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
